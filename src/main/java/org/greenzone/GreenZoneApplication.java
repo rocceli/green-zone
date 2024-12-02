c279cad3f9ea82a81233b473193553fd785bd083
rocceli <ngugielijah08@gmail.com>
2024-12-02 18:34:17 +0300

    Initial project setup

---
+++ src/main/java/org/greenzone/GreenZoneApplication.java
@@ -0,0 +1,13 @@
+package org.greenzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenZoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/greenzone/GreenZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.greenzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
